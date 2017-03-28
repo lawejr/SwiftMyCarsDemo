e5c5a83500ff21bcafc70f9efb744b885e9057f2
Aleksandr Kalinin <lawejr@gmail.com>
2017-03-28 13:07:21 +0500

    read data from external file and write to CoreData

--- MyCars/ViewController.swift
+++ MyCars/ViewController.swift
@@ -11,8 +11,8 @@ import CoreData
 
 class ViewController: UIViewController {
   
-  
   lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+  
   
   @IBOutlet weak var segmentedControl: UISegmentedControl!
   @IBOutlet weak var markLabel: UILabel!
@@ -33,6 +33,65 @@ class ViewController: UIViewController {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  
+  func getDataFromFile() {
+    let carsRequest: NSFetchRequest<Car> = Car.fetchRequest()
+    // Пример-пустышка, как делать запрос выборки данных по какому-то конкретному значению
+    carsRequest.predicate = NSPredicate(format: "mark !=nil")
+    
+    var records = 0
+    
+    do {
+      let count = try context.count(for: carsRequest)
+      records = count
+    } catch {
+      print(error.localizedDescription)
+    }
+    
+    guard records == 0 else { return }
+    // Путь до файла откуда брать данные
+    let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist")
+    let dataArray = NSArray(contentsOfFile: pathToFile!)!
+    
+    for dictionary in dataArray {
+      let carEntity = NSEntityDescription.entity(forEntityName: "Car", in: context)
+      assert(carEntity != nil, "carEntity == nil")
+      let carObject = NSManagedObject(entity: carEntity!, insertInto: context) as! Car
+      // Кастим словарь полученный в цикле до реального словаря
+      let carDictionary = dictionary as! NSDictionary
+      
+      carObject.mark = carDictionary["mark"] as? String
+      carObject.model = carDictionary["model"] as? String
+      carObject.rating = (carDictionary["rating"] as? Double)!
+      carObject.lastStarted = carDictionary["lastStarted"] as? NSDate
+      carObject.timesDriven = carDictionary["timesDriven"] as! Int16
+      carObject.myChoice = (carDictionary["myChoice"] as? Bool)!
+      
+      let imageName = carDictionary["imageName"] as? String
+      let image = UIImage(named: imageName!)
+      let imageData = UIImagePNGRepresentation(image!)
+      
+      carObject.imageData = imageData as NSData?
+      
+      let colorDictionary = carDictionary["tintColor"] as! NSDictionary
+      carObject.tintColor = getColor(colorDictionary: colorDictionary)
+      
+      do {
+        try context.save()
+      } catch {
+        print("Error \(error.localizedDescription)")
+      }
+    }
+  }
+  
+  func getColor(colorDictionary: NSDictionary) -> UIColor {
+    let red = colorDictionary["red"] as! NSNumber
+    let green = colorDictionary["green"] as! NSNumber
+    let blue = colorDictionary["blue"] as! NSNumber
+    
+    return UIColor(red: CGFloat(red)/255, green: CGFloat(green)/255, blue: CGFloat(blue)/255, alpha: 1.0)
   }
   
   
